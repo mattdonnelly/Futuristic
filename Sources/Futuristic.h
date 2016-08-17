6d2e1b6bc5ef0522087605e50f5e5f64cc8c88da
Matt Donnelly <mattdonnelly@me.com>
2016-08-17 22:41:12 +0100

    Initial commit

---
+++ Sources/Futuristic.h
@@ -0,0 +1,19 @@
+//
+//  Futuristic.h
+//  Futuristic
+//
+//  Created by Matt Donnelly on 17/08/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Futuristic.
+FOUNDATION_EXPORT double FuturisticVersionNumber;
+
+//! Project version string for Futuristic.
+FOUNDATION_EXPORT const unsigned char FuturisticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Futuristic/PublicHeader.h>
+
+
